7d495ae2c5a54cdbed7cff3efdfa17b8ca1d9e76
AdrianJakobsen <adrjak.aj@gmail.com>
2014-09-12 18:34:45 +0200

    woops det var her det egentlig var rydding

---
+++ Workshop/Skole mappe/src/SummeringAvHelTallSifre.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class SummeringAvHelTallSifre {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean exit = false;
+		while (!exit) {
+			System.out.println("\n Input one Whole number between 0 and 1000:");
+			int number = input.nextInt();
+			if ( number > 0 && number < 1000) {
+				int summering = (int) number % 10 + number % 100 / 10 + number /100;
+				System.out.println("The sum is: " + summering );
+			} 
+			else {
+
+				System.out.println("ERROR!!!");
+			} 
+		}
+		input.close();
+	}
+
+}
